4c7feec7bb912b25c3541440bc434d7adfc636dd
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-01-25 14:53:41 +0200

    Implement ordering, basic filtering and pagination for trips

--- api/routes/trips.js
+++ api/routes/trips.js
@@ -2,10 +2,71 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
+const table_name = 'trips';
+
+const VALID_ORDERINGS = [
+  'departure',
+  'return',
+  'departure_station_id',
+  'departure_station_name',
+  'return_station_id',
+  'return_station_name',
+  'covered_distance_(m)',
+  'duration_(sec.)',
+];
+
+const VALID_ORDERING_DIRECTIONS = ['asc', 'desc'];
+
+const DEFAULT_SORT_DIRECTION = VALID_ORDERING_DIRECTIONS[0];
+
+const validateSortBy = (oSortBy) => {
+  let sortBy = oSortBy;
+
+  if (!sortBy.includes(':')) {
+    sortBy = sortBy.concat(`:${DEFAULT_SORT_DIRECTION}`);
+  }
+
+  let [ordering, direction] = sortBy.toLowerCase().split(':');
+  if (
+    !VALID_ORDERINGS.includes(ordering) &&
+    !VALID_ORDERING_DIRECTIONS.includes(direction)
+  ) {
+    console.log(ordering);
+    console.log(direction);
+    throw new Error('Not a valid way to sort.');
+  } else {
+    return [ordering, direction];
+  }
+};
+
 const get = async (req, res, next) => {
   try {
-    db.all('SELECT * FROM trips LIMIT 10', (err, rows) => {
-      res.status(200).send(rows);
+    const sortBy = req.query.sort_by || 'Departure:Asc';
+    const page = req.query.page || 1;
+    const perPage = req.query.per_page || 10;
+    const offset = (page - 1) * perPage;
+
+    const field = req.query.field;
+    const value = req.query.value;
+
+    let query = `SELECT * FROM ${table_name} WHERE 1 = 1`;
+    let placeholders = [];
+    if (field && value) {
+      query += ` AND ${field} = ?`;
+      placeholders.push(value);
+    }
+
+    let [ordering, direction] = validateSortBy(sortBy);
+    query += ` ORDER BY ${ordering} ${direction}`;
+    query += ' LIMIT ? OFFSET ?';
+    placeholders.push(perPage, offset);
+
+    db.all(query, placeholders, (err, rows) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(rows);
+      }
     });
   } catch (e) {
     next(e);
